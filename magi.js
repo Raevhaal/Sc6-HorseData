9bb5f1d3b049fa8ec0c83f0765a2860ec354ae66
Horseface <33651860+aHorseface@users.noreply.github.com>
2022-03-31 16:19:37 +0200

    Localstorage and non dom init table

--- magi.js
+++ magi.js
@@ -8,73 +8,78 @@
 
 //Globals
 const FrameDataCSV = "https://docs.google.com/spreadsheets/d/1R3I_LXfqhvFjlHTuj-wSWwwqYmlUf299a3VY9pVyGEw/export?exportFormat=tsv";
-const Fheaders = ["Character", "Move category", "Move Name", "Stance", "Command", "Hit level", "Impact", "Damage", "Block", "Hit", "Counter Hit", "Guard Burst", "Notes"];
+
 var Fdata;
 var Dtable;
 
 
 function initFrameData(){
+    const CSVHeaders = ["Character", "Move category", "Move Name", "Stance", "Command", "Hit level", "Impact", "Damage", "Block", "Hit", "Counter Hit", "Guard Burst", "Notes"];
+
+    toastr.options = {
+        "closeButton": true,
+        "newestOnTop": false,
+        "positionClass": "toast-top-left",
+        "preventDuplicates": false,
+        "onclick": null,
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    }
+    toastr.warning("Getting framedata...");
+
     Papa.parse(FrameDataCSV, {
         download: true,
         header: true,
         delimiter: "\t",
         transformHeader: function(header,index){
             //Applies headers from Fheaders
-            return Fheaders[index];
+            return CSVHeaders[index];
         },
         //Might not be needed
         beforeFirstChunk: (chunk) => {
             //Removes the header of the csv file
-            chunk = chunk.split("*CSV LINE MARKER*,,,,,,,,,,,,,\r\n")[1]
+            chunk = chunk.split("*CSV LINE MARKER*,,,,,,,,,,,,,\r\n")[1];
             return chunk;
         },
         complete: (results) => {
+            //Pre process
             results.data.splice(0,3);
-            Fdata = results.data;
-            createTable(Fdata);
+
+            //Add framedata to localstorage
+            localStorage.setItem("Fdata", JSON.stringify(results.data));
+
+            createTable(results.data);
+            toastr.clear();
         }
     });
 
 }
 
 function createTable(data){
-    console.log("before");
-    var startHeader = '<thead>';
-    var endHeader = '</tr></thead><tbody>';
-    var endTable = '</tbody></table>'
-    
-    //Headers
-    var table = startHeader;
-    Fheaders.forEach(header => table += `<th scope="col">${header}</th>`);
-    table += endHeader
+    const Fheaders = ["Character", "Move category", "Move Name", "Stance", "Command", "Hit level", "Impact", "Damage", "Block", "Hit", "Counter Hit", "Guard Burst", "Notes"];
 
+    var vData = [];
     data.forEach(function (row, index){
-        if(index > 1000000){
-            return false; //break
-        } else {
-            table += `<tr>
-            <td scope="col">${row[Fheaders[0]]}</td>
-            <td scope="col">${row[Fheaders[1]]}</td>
-            <td scope="col">${row[Fheaders[2]]}</td>
-            <td scope="col">${row[Fheaders[3]]}</td>
-            <td scope="col">${row[Fheaders[4]]}</td>
-            <td scope="col">${row[Fheaders[5]]}</td>
-            <td scope="col">${row[Fheaders[6]]}</td>
-            <td scope="col">${row[Fheaders[7]]}</td>
-            <td scope="col">${row[Fheaders[8]]}</td>
-            <td scope="col">${row[Fheaders[9]]}</td>
-            <td scope="col">${row[Fheaders[10]]}</td>
-            <td scope="col">${row[Fheaders[11]]}</td>
-            <td scope="col">${row[Fheaders[12]]}</td>
-            </tr>`
-        }
+        vData.push([
+            row[Fheaders[0]],
+            row[Fheaders[1]],
+            row[Fheaders[2]],
+            row[Fheaders[3]],
+            row[Fheaders[4]],
+            row[Fheaders[5]],
+            row[Fheaders[6]],
+            row[Fheaders[7]],
+            row[Fheaders[8]],
+            row[Fheaders[9]],
+            row[Fheaders[10]],
+            row[Fheaders[11]],
+            row[Fheaders[12]],
+        ]);
     });
-
-    //End table
-    table = table + endTable;
-
-    //Start table
-    $('#fdata').html(table);
 
     var vDom = `
                 <"row mx-md-n5"
@@ -89,9 +94,26 @@ function createTable(data){
                     <"col-6"i>
                     <"col-6 float-right"p>
                 >
-    `
+    `;
     
     Dtable = $('#fdata').DataTable({
+        data: vData,
+        columns: [
+            { title: Fheaders[0] },
+            { title: Fheaders[1] },
+            { title: Fheaders[2] },
+            { title: Fheaders[3] },
+            { title: Fheaders[4] },
+            { title: Fheaders[5] },
+            { title: Fheaders[6] },
+            { title: Fheaders[7] },
+            { title: Fheaders[8] },
+            { title: Fheaders[9] },
+            { title: Fheaders[10] },
+            { title: Fheaders[11] },
+            { title: Fheaders[12] },
+        ],
+
         dom: vDom,
         buttons: [
             'colvis',
@@ -110,7 +132,8 @@ function createTable(data){
 
         searchBuilder: {
             columns: [3,4,5]
-        },  
+        },
+          
 
         colReorder: true,
         columnDefs: [
@@ -122,8 +145,14 @@ function createTable(data){
 
     });
 }
-
+var Test;
 $(document).ready(function() {
-    initFrameData();
+    if (localStorage.hasOwnProperty("Fdata")) {
+        createTable(JSON.parse(localStorage.Fdata));
+    } else {
+        initFrameData();
+    }
+      
+    
 });
     
---
+++ start test webserver.bat
@@ -0,0 +1,2 @@
+color a
+python -m http.server
