9456981088830ccfadd207bdd5df442726f2f7a5
Horseface <33651860+aHorseface@users.noreply.github.com>
2022-04-03 21:09:27 +0200

    Icon magic

--- magi.js
+++ magi.js
@@ -19,8 +19,32 @@ var Dtable;
 var CharacterFilter = "";
 var UrlFilter = "";
 
-
+//Icons
 var CommandIcons = [
+    //Slide inputs
+    [":a::A:", '<img width="28" height="20" src="icons/A_A.png" value = "aA"></img>'],
+    [":a::B:", '<img width="28" height="20" src="icons/A_B.png" value = "aB"></img>'],
+    [":a::K:", '<img width="28" height="20" src="icons/A_K.png" value = "aK"></img>'],
+    [":a::G:", '<img width="28" height="20" src="icons/A_G.png" value = "aG"></img>'],
+    
+    [":b::A:", '<img width="28" height="20" src="icons/B_A.png" value = "bA"></img>'],
+    [":b::B:", '<img width="28" height="20" src="icons/B_B.png" value = "bB"></img>'],
+    [":b::K:", '<img width="28" height="20" src="icons/B_K.png" value = "bK"></img>'],
+    [":b::G:", '<img width="28" height="20" src="icons/B_G.png" value = "bG"></img>'],
+    
+    [":k::A:", '<img width="28" height="20" src="icons/K_A.png" value = "kA"></img>'],
+    [":k::B:", '<img width="28" height="20" src="icons/K_B.png" value = "kB"></img>'],
+    [":k::K:", '<img width="28" height="20" src="icons/K_K.png" value = "kK"></img>'],
+    [":k::G:", '<img width="28" height="20" src="icons/K_G.png" value = "kG"></img>'],
+    
+    [":g::A:", '<img width="28" height="20" src="icons/G_A.png" value = "gA"></img>'],
+    [":g::B:", '<img width="28" height="20" src="icons/G_B.png" value = "gB"></img>'],
+    [":g::K:", '<img width="28" height="20" src="icons/G_K.png" value = "gK"></img>'],
+    [":g::G:", '<img width="28" height="20" src="icons/G_G.png" value = "gG"></img>'],
+
+    //B6B missing
+
+    //Directions
     [":9:", '<img width="20" height="20" src="icons/9.png" value = "9"></img>'],
     [":8:", '<img width="20" height="20" src="icons/8.png" value = "8"></img>'],
     [":7:", '<img width="20" height="20" src="icons/7.png" value = "7"></img>'],
@@ -40,27 +64,24 @@ var CommandIcons = [
     [":(2):", '<img width="20" height="20" src="icons/2_.png" value = "(2)"></img>'],
     [":(1):", '<img width="20" height="20" src="icons/1_.png" value = "(1)"></img>'],
 
+    //Buttons
     [":(G):", '<img width="20" height="20" src="icons/G_.png" value = "(G)"></img>'],
     [":G:", '<img width="20" height="20" src="icons/G.png" value = "G"></img>'],
-    [":g:", '<img width="20" height="20" src="icons/gs.png" value = "g"></img>'],
 
     [":(A):", '<img width="20" height="20" src="icons/A_.png" value = "(A)"></img>'],
     [":A:", '<img width="20" height="20" src="icons/A.png" value = "A"></img>'],
-    [":a:", '<img width="20" height="20" src="icons/As.png" value = "a"></img>'],
 
     [":(B):", '<img width="20" height="20" src="icons/B_.png" value = "(B)"></img>'],
     [":B:", '<img width="20" height="20" src="icons/B.png" value = "B"></img>'],
-    [":b:", '<img width="20" height="20" src="icons/Bs.png" value = "b"></img>'],
 
     [":(K):", '<img width="20" height="20" src="icons/K_.png" value = "(K)"></img>'],
     [":K:", '<img width="20" height="20" src="icons/K.png" value = "K"></img>'],
-    [":k:", '<img width="20" height="20" src="icons/Ks.png" value = "k"></img>'],
 
     [":A+K:", '<img width="46" height="20" src="icons/AK.png" value = "A+K"></img>'],
     [":(A)+(K):", '<img width="46" height="20" src="icons/AK_.png" value = "(A)+(K)"></img>'],
 
     [":A+B:", '<img width="46" height="20" src="icons/AB.png" value = "A+B"></img>'],
-    [":a+b:", '<img width="46" height="20" src="icons/AB.png" value = "A+B"></img>'],
+    [":a+b:", '<img width="46" height="20" src="icons/AB.png" value = "a+b"></img>'],
     [":(A)+(B):", '<img width="46" height="20" src="icons/AB_.png" value = "(A)+(B)"></img>'],
 
     [":K+G:", '<img width="46" height="20" src="icons/KG.png" value = "K+G"></img>'],
@@ -80,13 +101,27 @@ var CommandIcons = [
     [":A+B+K:", '<img width="72" height="20" src= "icons/ABK.png" value = "A+B+K"></img>'],
     [":(A)+(B)+(K):", '<img width="72" height="20" src="icons/ABK_.png" value = "(A+B+K)"></img>'],
 
+    [":g:", '<img width="20" height="20" src="icons/gs.png" value = "g"></img>'],
+    [":a:", '<img width="20" height="20" src="icons/As.png" value = "a"></img>'],
+    [":b:", '<img width="20" height="20" src="icons/Bs.png" value = "b"></img>'],
+    [":k:", '<img width="20" height="20" src="icons/Ks.png" value = "k"></img>'],
+
     //["\_", '<img width="72" height="20" src="icons/underscore.png" value = "underscore"></img>'],
 
+];
+var HeightIcons = [
+    [":H:", '<img width="20" height="20" src="icons/H.png" value = "H"></img>'],
+    [":M:", '<img width="20" height="20" src="icons/M.png" value = "M"></img>'],
+    [":L:", '<img width="20" height="20" src="icons/L.png" value = "L"></img>'],
+
+    [":SH:", '<img width="40" height="20" src="icons/SH.png" value = "SH"></img>'],
+    [":SM:", '<img width="40" height="20" src="icons/SM.png" value = "SM"></img>'],
+    [":SL:", '<img width="40" height="20" src="icons/SL.png" value = "SL"></img>'],
 ];
 
 function downloadFrameData(){
     const CSVHeaders = ["Character", "Move category", "Move Name", "Stance", "Command", "Hit level", "Impact", "Damage", "Block", "Hit", "Counter Hit", "Guard Burst", "Notes"];
-    toastr.warning("Getting framedata...");
+    toastr.warning("Downloading framedata...");
 
     Papa.parse(FrameDataCSV, {
         download: true,
@@ -95,12 +130,6 @@ function downloadFrameData(){
         transformHeader: function(header,index){
             //Applies headers from Fheaders
             return CSVHeaders[index];
-        },
-        //Might not be needed
-        beforeFirstChunk: (chunk) => {
-            //Removes the header of the csv file
-            chunk = chunk.split("*CSV LINE MARKER*,,,,,,,,,,,,,\r\n")[1];
-            return chunk;
         },
         complete: (results) => {
             //Pre process
@@ -185,8 +214,10 @@ function createTable(data){
         pageLength: 20,
         lengthMenu: [[10, 15, 20, 30, 40, 50, -1], [10, 15, 20, 30, 40, 50, "All"]],
 
-        scrollX: false,
-        scrollY: false,
+        scrollCollapse: true,
+        scrollX: true,
+        //scrollY: '1vh',
+        scrollY: true,
           
         colReorder: {
             realtime: false
@@ -196,7 +227,9 @@ function createTable(data){
             {targets: 1, visible: false},//Move category
             {targets: 2, visible: false},//Move Name
             {
-                targets: 4, visible: true,
+                targets: 4, 
+                visible: true,
+                width: "10%",
                 render: function(data, type, row){
                     value = data;
                     for (let index = 0; index < CommandIcons.length; index++) {
@@ -205,16 +238,16 @@ function createTable(data){
                     return value;
                 }
             },//Command
-            // {
-            //     targets: 5, visible: true,
-            //     render: function(data, type, row){
-            //         value = data;
-            //         for (let index = 0; index < CommandIcons.length; index++) {
-            //             value = value.replaceAll(CommandIcons[index][0], CommandIcons[index][1])
-            //         }
-            //         return value;
-            //     }
-            // },//Input
+            {
+                targets: 5, visible: true,
+                render: function(data, type, row){
+                    value = data;
+                    for (let index = 0; index < HeightIcons.length; index++) {
+                        value = value.replaceAll(HeightIcons[index][0], HeightIcons[index][1])
+                    }
+                    return value;
+                }
+            },//Input
             {targets: 7, type: 'numeric-comma'},//Damage
             {targets: 12, visible: true},//Notes
         ],
